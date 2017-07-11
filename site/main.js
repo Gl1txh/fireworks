d529e10a9d0380c204d4a10e0a3ecd1e25a692be
Mariusz Dabrowski <wayrse@gmail.com>
2017-07-11 14:55:21 -0400

    Trigger points added
    - The fireworks now come out of the correct places on the tower

--- site/main.js
+++ site/main.js
@@ -7,22 +7,92 @@ var canvas = document.querySelector('canvas');
 		canvas.height = document.body.clientHeight * 2;
 var context = canvas.getContext('2d');
 
-var fireworkCount = 1;
 var fireworks = [];
 var gravity = new vec2(0, 0.2);
 
+
+
+// --------------
+// Setup triggers
+// --------------
+
+var triggers = {};
+var triggerInfo = [
+	// id, velocity, lifespan
+	['#trigger-hat-left', {x: -10, y: -6}, 80],
+	['#trigger-hat-right', {x: 10, y: -6}, 80],
+	['#trigger-top-left', {x: -10, y: -6}, 130],
+	['#trigger-top-right', {x: 10, y: -6}, 130],
+	['#trigger-middle-left', {x: -10, y: -2}, 200],
+	['#trigger-middle-right', {x: 10, y: -2}, 200],
+	['#trigger-bottom-left', {x: -10, y: 2}, 130],
+	['#trigger-bottom-right', {x: 10, y: 2}, 130]
+];
+
+function Trigger(id, velocity, lifespan) {
+	this.element = document.querySelector(id);
+	this.info = this.element.getBoundingClientRect();
+	this.top = this.info.top * 2;
+	this.left = this.info.left * 2;
+	this.active = false;
+	this.velocity = velocity;
+	this.lifespan = lifespan;
+}
+
+for(var i = 0; i < triggerInfo.length; i++) {
+	triggers[triggerInfo[i]] = new Trigger(triggerInfo[i][0], triggerInfo[i][1], triggerInfo[i][2]);
+}
+
+
+
+// -------------
+// Key listeners
+// -------------
+
+function triggerFireworks() {
+	for(var i = 0; i < arguments.length; i++) {
+		var y = triggers[triggerInfo[arguments[i]]].top;
+		var x = triggers[triggerInfo[arguments[i]]].left;
+		var velocity = triggers[triggerInfo[arguments[i]]].velocity;
+		var lifespan = triggers[triggerInfo[arguments[i]]].lifespan;
+
+		fireworks.push(new Firework(x, y, velocity, lifespan, true)); // -5 is half the radius of the particle
+	}
+}
+
+function onKeyDown(evt) {
+	var key = evt.keyCode;
+	switch (key) {
+		case 65:
+			triggerFireworks(0, 1);
+			break;
+		case 83:
+			triggerFireworks(2, 3);
+			break;
+		case 68:
+			triggerFireworks(4, 5);
+			break;
+		case 70:
+			triggerFireworks(6, 7);
+			break;
+	}
+}
+
+document.addEventListener("keyup", onKeyDown);
 
 
 // --------------------
 // Particle constructor
 // --------------------
 
-function Particle(x, y, firework) {
+function Particle(x, y, velocity, firework) {
 	this.position = new vec2(x, y);
 	this.lifespan = 100;
 	this.radius = (firework) ? 10 : 5;
 	if(firework) {
-		this.velocity = new vec2(0, Math.random() * (-20 - -10) + -10);
+		var temp = new vec2();
+		this.velocity = new vec2();
+		this.velocity.add(velocity);
 	} else {
 		this.velocity = new vec2(
 			Math.random() * (10 - -10) + -10,
@@ -68,17 +138,19 @@ function Particle(x, y, firework) {
 // Firework constructor
 // --------------------
 
-function Firework(x, y, firework) {
-	this.firework = new Particle(x, y, firework);
+function Firework(x, y, velocity, lifespan, firework) {
+	this.firework = new Particle(x, y, velocity, firework);
 	this.exploded = false;
 	this.particles = [];
+	this.lifespan = lifespan;
 
 	this.update = function() {
 		if(!this.exploded) {
 			this.firework.update();
 			this.firework.applyForce(gravity);
+			this.lifespan -= 5;
 
-			if(this.firework.velocity.y > 0) {
+			if(this.lifespan < 0) {
 				this.exploded = true;
 				this.explode();
 			}
@@ -121,11 +193,6 @@ function Firework(x, y, firework) {
 	};
 }
 
-//
-// for(var i = 0; i < fireworkCount; i++) {
-// 	fireworks.push(new Firework(Math.random() * canvas.width - 5, canvas.height - 10, true)); // -5 is half the radius of the particle
-// }
-
 
 
 // ------------
@@ -140,11 +207,6 @@ function update() {
 		if(fireworks[i].done()) {
 			fireworks.splice(i, 1);
 		}
-	}
-
-	if(Math.random() < 0.1) {
-		fireworks.push(new Firework(Math.random() * canvas.width - 10, canvas.height + 10, true)); // -5 is half the radius of the particle
-		console.log(fireworks.length);
 	}
 
 	// Render
@@ -185,6 +247,14 @@ requestAnimationFrame(update);
 // -------------
 
 window.addEventListener('resize', function() {
+
+	// Update the canvas size
 	canvas.width = document.body.clientWidth * 2;
 	canvas.height = document.body.clientHeight * 2;
+
+	// Update the trigger position
+	triggers = {};
+	for(var i = 0; i < triggerInfo.length; i++) {
+		triggers[triggerInfo[i]] = new Trigger(triggerInfo[i][0], triggerInfo[i][1]);
+	}
 });
